7969af7261af9212dd4d7ec98a7e9a5a38b8e4d9
Showner <dev@showner.fr>
2021-08-07 16:19:44 +0200

    Update brewfile, zshrc too

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+# Pre-requise
+
+Have [Homebrew](https://brew.sh/) installed
+
+# Install
+
+Clone the repo, have tmux and zsh installed,
+
+execute `./install.sh`
+
+
+
+Inspired by many dotfiles and tutos from :globe_with_meridians:
+
 Made with :heart: by Showner
--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ export TMUXDIR=$DOTFILE_FOLDER/tmux
 # Check zshrc presence
 if [ -e $ZSHDIR/zshrc ]; then
   if [ -e $HOME/.zshrc ]; then
-    echo "Deleting old zshrc symlink ..."
+    echo "Deleting old zshrc file ..."
     rm $HOME/.zshrc
   fi
   echo "Creating symlink to zshrc ..."
@@ -17,7 +17,7 @@ fi
 # Check tmux presence
 if [ -e $TMUXDIR/tmux.conf ]; then
   if [ -e $HOME/.tmux.conf ]; then
-    echo "Deleting old .tmux.conf symlink ..."
+    echo "Deleting old .tmux.conf file ..."
     rm $HOME/.tmux.conf
   fi
   echo "Creating symlink to tmux.conf ..."
