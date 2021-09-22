070f96fb0e0a22eda8b4e19d00e914996db0eea0
Showner <dev@showner.fr>
2021-09-22 19:45:02 +0200

    total rework org

--- upgrade_alias.sh
+++ upgrade_alias.sh
@@ -10,17 +10,12 @@ if [ -e $ZSHDIR/zshrc ]; then
     echo "Deleting old zshrc file ..."
     rm $HOME/.zshrc
   fi
+  if [ -e $ZSHDIR/zshrc ]; then
+    mv $ZSHDIR/conf/aliases.local $ZSHDIR/conf/conf.local
+  fi
   echo "Creating symlink to zshrc ..."
   ln -s $ZSHDIR/zshrc $HOME/.zshrc
+
   printf "\e[1;37mDon't forget to :\e[0m\nsource ~/.zshrc\n"
 fi
 
-# Check tmux presence
-if [ -e $TMUXDIR/tmux.conf ]; then
-  if [ -e $HOME/.tmux.conf ]; then
-    echo "Deleting old .tmux.conf file ..."
-    rm $HOME/.tmux.conf
-  fi
-  echo "Creating symlink to tmux.conf ..."
-  ln -s $TMUXDIR/tmux.conf $HOME/.tmux.conf
-fi
